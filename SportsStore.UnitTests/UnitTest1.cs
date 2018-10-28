d2651e76469a63f1e81c8442d75d425408781196
Pawel Kalinowski <pawel_kalinowski@onet.pl>
2018-10-29 00:59:25 +0100

    nav babkend, improve routing

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -31,8 +31,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
             // działanie
 
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
-
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
 
             // asercje
             Product[] prodArray = result.Products.ToArray();
@@ -68,6 +67,7 @@ namespace SportsStore.UnitTests
                 + @"<a class=""btn btn-default"" href=""Strona3"">3</a>",
                 result.ToString());
         }
+
         [TestMethod]
         public void Can_Send_Pagination_View_Model()
         {
@@ -83,15 +83,68 @@ namespace SportsStore.UnitTests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             //dzialanie
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
             //asercje
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
             Assert.AreEqual(pageInfo.ItemsPerPage, 3);
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // przygotowanie
+            // - utworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+            //przygotowanie - utworzenie kontrolera i ustawienie 3-elementowej strony
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //działanie
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            //assercje
+            Assert.AreEqual(2, result.Length);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
 
         }
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // przygotowanie
+            // - utworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+                new Product {ProductID = 2, Name = "P2", Category = "Jabłka"},
+                new Product {ProductID = 3, Name = "P3", Category = "Śliwki"},
+                new Product {ProductID = 4, Name = "P4", Category = "Pomarańcze"},
+            });
+
+            //przygotowanie utworzenie kontrolera
+            NavController controller = new NavController(mock.Object);
+            //działanie
+            string[] results = ((IEnumerable<string>)controller.Menu().Model).ToArray();
+
+            //assercje
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Jabłka");
+            Assert.AreEqual(results[1], "Pomarańcze");
+            Assert.AreEqual(results[2], "Śliwki");
+
+
+        }
+
 
     }
 }
